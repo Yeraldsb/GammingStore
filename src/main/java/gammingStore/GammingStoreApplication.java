9a45b1a8749ca521053f869b297911309e055f24
Yeraldsb <pamelasb9412@gmail.com>
2021-06-07 10:34:00 +0200

    Initial commit

---
+++ src/main/java/gammingStore/GammingStoreApplication.java
@@ -0,0 +1,13 @@
+package gammingStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GammingStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GammingStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gammingStore/GammingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package gammingStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GammingStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
