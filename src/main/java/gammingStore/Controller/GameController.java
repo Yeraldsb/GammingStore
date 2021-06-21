4d23f526aa469c73ac5ecf81ffe49f106c5b9189
rossyrom84 <rosangely.romero@gmail.com>
2021-06-21 15:34:08 +0200

    restructuración edit-new-home

--- src/main/java/gammingStore/Controller/GameController.java
+++ src/main/java/gammingStore/Controller/GameController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
 
 @Controller
 public class GameController {
@@ -21,19 +20,12 @@ public class GameController {
         this.gameService = gameService;
     }
 
-    @GetMapping("/games")
-    String listGames (Model model) {
-        List<Game> games = gameService.allGames();
-        model.addAttribute("title", "Game list");
-        model.addAttribute("games", games);
-        return "games/all";
-    }
     @GetMapping("/games/new")
     String newGame(Model model) {
         Game game = new Game();
         model.addAttribute("game", game);
         model.addAttribute("title", "Create new game");
-        return "games/new";
+        return "games/edit";
 
     }
     @GetMapping("/games/edit/{id}")
@@ -46,23 +38,12 @@ public class GameController {
     @GetMapping("games/delete/{id}")
     public String remove(@PathVariable Long id) {
         gameService.delete(id);
-        return "redirect:/games";
+        return "redirect:/home";
     }
     @PostMapping("/games/new")
     public String addGame(@ModelAttribute Game game) {
         gameService.save(game);
-        return "redirect:/games";
+        return "redirect:/home";
     }
-
-
-
-    /*(document).ready(function(){
-        $(".form-control").on("keyup", function() {
-            var value = $(this).val().toLowerCase();
-            $(".form-control tr").filter(function() {
-                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-            });
-        });
-    });*/
 }
 
